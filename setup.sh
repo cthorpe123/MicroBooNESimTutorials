b66e2c48da02896b658336abf6673d2fc30fe43f
cthorpe123 <c.thorpe@lancaster.ac.uk>
2022-05-06 15:15:05 -0500

    first commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+source /grid/fermiapp/products/uboone/setup_uboone.sh
+#source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+
+setup uboonecode v08_00_00_61 -q e17:prof
+
+#unsetup mrb
+#setup mrb -o
